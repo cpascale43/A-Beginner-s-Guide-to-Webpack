b1e22fe62b4b122b85158b527cd1e5e85b2d77e0
Carrie Pascale <cpascale43@gmail.com>
2020-06-08 20:24:47 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// const click = require('./game');
+import click from './game';
+
+const button = document.getElementById('button');
+
+button.addEventListener('click', function() {
+  click();
+});
